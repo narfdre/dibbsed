b860f14af55fb8199f4a8ad8f758e88242163fcb
Andre Kradolfer <narfdre@gmail.com>
2011-08-28 12:07:06 -0600

    Proof Of Concept using NodeJS
    npm managed, MongoDB required with db named "dibbsed",

---
+++ app.js
@@ -0,0 +1,57 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    mongoose = require('mongoose'),
+    	db = mongoose.connect('mongodb://localhost/dibbsed'),
+	schema = mongoose.Schema;
+	
+ 	var dibb = new schema({
+     	//id : {type: Number, unique : true},
+     	user : String,
+      	name : String,
+      	date : { type: Date, default: Date.now },
+      	tags : [String]
+	});
+var Dibb = mongoose.model('dibb', dibb);
+var firstDibb = new Dibb();
+firstDibb.user = "narfdre";
+firstDibb.name = "Milk";
+firstDibb.date = Date.now();
+firstDibb.save();
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+//  res.render('index', {
+//   title: 'Express'
+//  });
+});
+
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+Dibb.find({}, function(err, docs){
+  console.log(docs);
+});
