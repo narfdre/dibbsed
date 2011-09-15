3b0c75f80ccf5759afb63084b3730b4f843c3105
Andre Kradolfer <narfdre@gmail.com>
2011-09-14 21:34:02 -0600

    adding user model

--- model.js
+++ model.js
@@ -10,6 +10,13 @@ var dibb = new schema({
       	tags : [String]
 });
 
+var user = new schema({
+		id: objectId,
+		name: String,
+		email: { type: String, unqiue: true},
+		twitter: { type: String, unqiue: true, required: false},
+});
+
 exports.getDibb = function(){
 	return mongoose.model('dibb', dibb);
 }
